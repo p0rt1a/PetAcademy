8804639981ab4da2f10b8082c98be36979426419
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-17 20:17:36 +0300

    Defined: HttpClientModule

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { CategoriesComponent } from './categories/categories.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CategoriesComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ClientApp/src/app/categories/categories.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Pet } from '../Models';
+import { PetsService } from '../_services/pets.service';
+
+@Component({
+  selector: 'categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css'],
+})
+export class CategoriesComponent implements OnInit {
+  pets?: Pet[];
+
+  constructor(private petsService: PetsService) {}
+
+  ngOnInit(): void {
+    this.getPets();
+  }
+
+  getPets() {
+    this.petsService.getPets().subscribe((response) => {
+      console.log(response);
+    });
+  }
+}
