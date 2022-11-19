bbf396a47637975819de2bcab21f3ed6231c32b5
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-19 11:56:52 +0300

    Added: Data transfer object for trainingcategories table

---
+++ ServerApp/DTO/AddTrainingDTO.cs
@@ -0,0 +1,14 @@
+﻿using ServerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.DTO
+{
+    public class AddTrainingDTO
+    {
+        public Training training { get; set; }
+        public int[] categoryIndexes { get; set; }
+    }
+}
