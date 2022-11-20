38384fc3c7412210b1d9e76a0f68d798f69b1ba2
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-20 22:18:48 +0300

    Added: Routing & Forms Module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,10 +6,36 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { CategoriesComponent } from './categories/categories.component';
+import { TrainingsComponent } from './trainings/trainings.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TrainersComponent } from './trainers/trainers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, CategoriesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    TrainingsComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    NotfoundComponent,
+    NavbarComponent,
+    TrainersComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
