8770c2b47f73395f1532c78368b30ec197693209
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-20 12:20:47 +0300

    Edit: Categories services methods

---
+++ ClientApp/src/app/_services/categories.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../Models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  baseUrl: string = 'https://localhost:5001/api/categories/';
+
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl);
+  }
+}
