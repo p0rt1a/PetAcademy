1ddcdaee6efca9085f11cd0affbc6dd07ff0c79f
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-20 22:17:07 +0300

    Build

---
+++ ServerApp/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Models
+{
+    public class Role : IdentityRole<int>
+    {
+    }
+}
---
+++ ServerApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Models
+{
+    public class User : IdentityUser<int>
+    {
+        public string Name { get; set; }
+    }
+}
