56d446992498dba3550ce6d2106a24de14cc1e9e
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-20 22:17:51 +0300

    Update: Deleted some models not necessary

--- ClientApp/src/app/Models.ts
+++ ClientApp/src/app/Models.ts
@@ -1,5 +1,5 @@
-export class Pet {
-  id: number;
+export class Category {
+  id?: number;
   name: string;
 
   constructor(id: number, name: string) {
@@ -7,3 +7,35 @@ export class Pet {
     this.name = name;
   }
 }
+
+export class AddTrainingDTO {
+  training: Training;
+  categoryIndexes: number[];
+
+  constructor(training: Training, categoryIndexes: number[]) {
+    this.training = training;
+    this.categoryIndexes = categoryIndexes;
+  }
+}
+
+export class Training {
+  header: string;
+  videoUrl: string;
+  description: string;
+
+  constructor(header: string, videoUrl: string, description: string) {
+    this.header = header;
+    this.videoUrl = videoUrl;
+    this.description = description;
+  }
+}
+
+// export class TrainingCategory {
+//   trainingId: number;
+//   categoryId: number;
+
+//   constructor(trainingId: number, categoryId: number) {
+//     this.trainingId = trainingId;
+//     this.categoryId = categoryId;
+//   }
+// }
