823a50ebfa75b122c66637740112eac7490c84c3
Alperen Polat <m_alperenPolat@hotmail.com>
2022-11-28 17:23:17 +0300

    Routes created and paths declared

--- ClientApp/src/app/routes.ts
+++ ClientApp/src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { AddTrainingComponent } from './add-training/add-training.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { RegisterComponent } from './register/register.component';
 import { TrainersComponent } from './trainers/trainers.component';
 import { TrainingsComponent } from './trainings/trainings.component';
+import { UpdateTrainingComponent } from './update-training/update-training.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +14,8 @@ export const appRoutes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'trainings', component: TrainingsComponent },
+  { path: 'update-training', component: UpdateTrainingComponent },
+  { path: 'add-training', component: AddTrainingComponent },
   { path: 'trainers', component: TrainersComponent },
   { path: '**', component: NotfoundComponent },
 ];
