56c098dc304b07dd4a653667b129e24ec8c6854b
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 00:11:22 +0300

    Create training controller with base functionalities

---
+++ ServerApp/WebApi/Controllers/TrainingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.DbOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class TrainingController : ControllerBase
+    {
+        private readonly IAcademyDbContext _context;
+
+        public TrainingController(IAcademyDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetTrainings()
+        {
+            return Ok();
+        }
+    }
+}
