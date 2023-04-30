e00940b62599cfe38e42f67e2d4f8fb2ae8b66ce
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-04-30 22:45:57 +0300

    Create academy database context

---
+++ ServerApp/WebApi/DbOperations/AcademyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.DbOperations
+{
+    public class AcademyDbContext : DbContext, IAcademyDbContext
+    {
+        public AcademyDbContext(DbContextOptions<AcademyDbContext> options) : base(options)
+        {
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ ServerApp/WebApi/DbOperations/IAcademyDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace WebApi.DbOperations
+{
+    public interface IAcademyDbContext
+    {
+        int SaveChanges();
+    }
+}
