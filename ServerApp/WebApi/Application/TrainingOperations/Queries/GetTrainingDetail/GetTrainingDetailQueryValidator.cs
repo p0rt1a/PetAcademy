d8f6a6573ee78c9cb06dcaee5e5c2e0ad7048aad
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 12:11:57 +0300

    Create validation rules for get training detail query

---
+++ ServerApp/WebApi/Application/TrainingOperations/Queries/GetTrainingDetail/GetTrainingDetailQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.TrainingOperations.Queries.GetTrainingDetail
+{
+    public class GetTrainingDetailQueryValidator : AbstractValidator<GetTrainingDetailQuery>
+    {
+        public GetTrainingDetailQueryValidator()
+        {
+            RuleFor(query => query.TrainingId).GreaterThan(0);
+        }
+    }
+}
