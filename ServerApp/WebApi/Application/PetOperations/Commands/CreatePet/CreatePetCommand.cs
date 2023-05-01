1d70711c491437bf574bc3d9fa3e5724e95258e8
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 23:11:32 +0300

    Create create pet command

---
+++ ServerApp/WebApi/Application/PetOperations/Commands/CreatePet/CreatePetCommand.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.PetOperations.Commands.CreatePet
+{
+    public class CreatePetCommand
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public CreatePetModel Model { get; set; }
+
+        public CreatePetCommand(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var user = _dbContext.Users.SingleOrDefault(x => x.Id == Model.UserId);
+
+            if (user is null)
+                throw new InvalidOperationException("Kullanıcı mevcut değil");
+
+            var genre = _dbContext.Genres.SingleOrDefault(x => x.Id == Model.GenreId);
+
+            if (genre is null)
+                throw new InvalidOperationException("Tür mevcut değil");
+
+            //TODO: Create pet with using mapper
+        }
+    }
+
+    public class CreatePetModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int GenreId { get; set; }
+        public int UserId { get; set; }
+    }
+}
