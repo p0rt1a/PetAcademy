780a23d6677071b444916e2dc261f2e4bd95a8f6
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 15:04:03 +0300

    Create enrollments entity as PetTrainings table

---
+++ ServerApp/WebApi/Entities/Enrollments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Enrollments
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+        public int TrainingId { get; set; }
+        public Training Training { get; set; }
+    }
+}
