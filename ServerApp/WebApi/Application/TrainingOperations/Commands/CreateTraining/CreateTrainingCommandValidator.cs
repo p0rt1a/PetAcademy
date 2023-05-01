4ef2603b61a8892522f1db1ce5e7f4441362c93d
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 13:31:12 +0300

    Create validator for create training command

---
+++ ServerApp/WebApi/Application/TrainingOperations/Commands/CreateTraining/CreateTrainingCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.TrainingOperations.Commands.CreateTraining
+{
+    public class CreateTrainingCommandValidator : AbstractValidator<CreateTrainingCommand>
+    {
+        public CreateTrainingCommandValidator()
+        {
+            RuleFor(command => command.Model.Title.Trim()).MinimumLength(8);
+            RuleFor(command => command.Model.Description.Trim()).MinimumLength(10);
+            RuleFor(command => command.Model.City.Trim()).MinimumLength(2);
+            RuleFor(command => command.Model.Address.Trim()).MinimumLength(2);
+            RuleFor(command => command.Model.Price).GreaterThan(0);
+        }
+    }
+}
