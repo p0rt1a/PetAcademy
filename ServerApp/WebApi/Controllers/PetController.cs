34af8f61ed2fbc9fe1ed52bf3146e06f0e2c6104
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 23:13:44 +0300

    Create create pet method in controller

--- ServerApp/WebApi/Controllers/PetController.cs
+++ ServerApp/WebApi/Controllers/PetController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Application.PetOperations.Commands.CreatePet;
 using WebApi.DbOperations;
 
 namespace WebApi.Controllers
@@ -20,6 +21,17 @@ namespace WebApi.Controllers
         {
             _context = context;
             _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreatePet([FromBody]CreatePetModel model)
+        {
+            CreatePetCommand command = new(_context, _mapper);
+            command.Model = model;
+
+            command.Handle();
+
+            return Ok();
         }
     }
 }
