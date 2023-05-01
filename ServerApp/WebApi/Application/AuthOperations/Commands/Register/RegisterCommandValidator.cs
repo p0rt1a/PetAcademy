0956f847c212989822ad3940264c8171077ca44a
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 21:57:04 +0300

    Create validator and set register command rules

---
+++ ServerApp/WebApi/Application/AuthOperations/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.AuthOperations.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(command => command.Model.Email.Trim()).NotEmpty().MinimumLength(8);
+            RuleFor(command => command.Model.Name.Trim()).NotEmpty().MinimumLength(2);
+            RuleFor(command => command.Model.Surname.Trim()).NotEmpty().MinimumLength(2);
+            RuleFor(command => command.Model.Password.Trim()).NotEmpty().MinimumLength(8);
+        }
+    }
+}
