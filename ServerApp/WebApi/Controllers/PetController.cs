0fa38e33a232557c63eb7a1948d4155bcd24cca0
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 23:01:02 +0300

    Create pet controller

---
+++ ServerApp/WebApi/Controllers/PetController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class PetController : ControllerBase
+    {
+        private readonly IAcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public PetController(IAcademyDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+    }
+}
