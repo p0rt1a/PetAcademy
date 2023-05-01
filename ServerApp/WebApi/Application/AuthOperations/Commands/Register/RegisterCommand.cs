f868186c43a53a5a7709cd1e14a91d61ea34d95d
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 21:49:45 +0300

    Create register command

---
+++ ServerApp/WebApi/Application/AuthOperations/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.AuthOperations.Commands.Register
+{
+    public class RegisterCommand
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RegisterCommand(IAcademyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Handle()
+        {
+            //TODO: Register user with using mapper
+        }
+    }
+
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
