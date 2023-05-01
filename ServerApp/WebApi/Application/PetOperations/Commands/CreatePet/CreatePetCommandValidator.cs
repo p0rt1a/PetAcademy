d7b3b29a172623f3b9330ccbdbc8ae0adcaf8fba
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 23:15:20 +0300

    Create validator for create pet command and create rules

---
+++ ServerApp/WebApi/Application/PetOperations/Commands/CreatePet/CreatePetCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.PetOperations.Commands.CreatePet
+{
+    public class CreatePetCommandValidator : AbstractValidator<CreatePetCommand>
+    {
+        public CreatePetCommandValidator()
+        {
+            RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.UserId).GreaterThan(0);
+            RuleFor(command => command.Model.Name.Trim()).MinimumLength(2);
+            RuleFor(command => command.Model.Age).GreaterThan(0);
+        }
+    }
+}
