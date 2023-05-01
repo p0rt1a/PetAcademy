3ab4b4f5998fdf901096e4a821de48fe8858580c
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 23:27:07 +0300

    Create validator for update pet command and create its rules

---
+++ ServerApp/WebApi/Application/PetOperations/Commands/UpdatePet/UpdatePetCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.PetOperations.Commands.UpdatePet
+{
+    public class UpdatePetCommandValidator : AbstractValidator<UpdatePetCommand>
+    {
+        public UpdatePetCommandValidator()
+        {
+            RuleFor(command => command.Model.UserId).GreaterThan(0);
+            RuleFor(command => command.PetId).GreaterThan(0);
+        }
+    }
+}
