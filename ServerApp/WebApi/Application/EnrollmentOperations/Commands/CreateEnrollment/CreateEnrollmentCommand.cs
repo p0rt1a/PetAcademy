61a2aae2bae6db0e09379ddaafa0a415bf585d78
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 15:19:13 +0300

    Create create enrollment command and create enrollment moedl

---
+++ ServerApp/WebApi/Application/EnrollmentOperations/Commands/CreateEnrollment/CreateEnrollmentCommand.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.EnrollmentOperations.Commands.CreateEnrollment
+{
+    public class CreateEnrollmentCommand
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public CreateEnrollmentCommand(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            //TODO: Add enrollment controls and throw error
+
+            //TODO: Map model to enrollment and save
+        }
+    }
+
+    public class CreateEnrollmentModel
+    {
+        public int PetId { get; set; }
+        public int TrainingId { get; set; }
+    }
+}
