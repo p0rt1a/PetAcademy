07389b17f02193803d0f6c9d738a35cb6fce5efe
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 23:36:14 +0300

    Create validator for login command

---
+++ ServerApp/WebApi/Application/AuthOperations/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.AuthOperations.Commands.Login
+{
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            RuleFor(command => command.Model.Email.Trim()).NotEmpty().MinimumLength(8);
+            RuleFor(command => command.Model.Password.Trim()).NotEmpty().MinimumLength(8);
+        }
+    }
+}
