22bb446d23e80dcd6bba4311f66c4ac63de21a55
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 14:56:15 +0300

    Create get genres query

---
+++ ServerApp/WebApi/Application/GenreOperations/Queries/GetGenres/GetGenresQuery.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.GenreOperations.Queries.GetGenres
+{
+    public class GetGenresQuery
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetGenresQuery(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            //TODO: Get genres with using mapper
+        }
+    }
+
+    public class GenreViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
