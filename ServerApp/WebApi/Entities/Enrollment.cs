b3626ba4446c87e17b7d57585dd67520a9081f67
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 15:05:04 +0300

    Fix wrong naming

--- ServerApp/WebApi/Entities/Enrollment.cs
+++ ServerApp/WebApi/Entities/Enrollment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebApi.Entities
 {
-    public class Enrollments
+    public class Enrollment
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
