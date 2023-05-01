cedc9587c112cf4ab90e49ec812c12db8789c0f8
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 15:16:08 +0300

    Create enrollment controller

---
+++ ServerApp/WebApi/Controllers/EnrollmentController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class EnrollmentController : ControllerBase
+    {
+        private readonly IAcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public EnrollmentController(IAcademyDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }        
+    }
+}
