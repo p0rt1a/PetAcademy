a704343dee165e15ddedf7f1632fec8caac4d7d9
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 13:46:42 +0300

    Create validator for update training command and set rules for it

---
+++ ServerApp/WebApi/Application/TrainingOperations/Commands/UpdateTraining/UpdateTrainingCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.TrainingOperations.Commands.UpdateTraining
+{
+    public class UpdateTrainingCommandValidator : AbstractValidator<UpdateTrainingCommand>
+    {
+        public UpdateTrainingCommandValidator()
+        {
+            RuleFor(command => command.TrainingId).GreaterThan(0);
+        }
+    }
+}
