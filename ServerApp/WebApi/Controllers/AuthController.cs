678da32542797183252ad327127c6b86c91cb3bd
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 21:46:53 +0300

    Create auth controller

---
+++ ServerApp/WebApi/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuthController(IAcademyDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+    }
+}
