93ff1dd80f80b981f782b24e1925309fe5f1699e
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 22:20:13 +0300

    Fix user email & password check if statement in login command

--- ServerApp/WebApi/Application/AuthOperations/Commands/Login/LoginCommand.cs
+++ ServerApp/WebApi/Application/AuthOperations/Commands/Login/LoginCommand.cs
@@ -25,7 +25,7 @@ namespace WebApi.Application.AuthOperations.Commands.Login
         {
             var user = _dbContext.Users.SingleOrDefault(x => x.Email == Model.Email && x.Password == Model.Password);
 
-            if (user is null)
+            if (user is not null)
                 throw new InvalidOperationException("Email ya da şifre hatalı");
 
             TokenHandler tokenHandler = new(_configuration);
