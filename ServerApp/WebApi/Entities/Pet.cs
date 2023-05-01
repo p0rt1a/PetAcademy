9e65743a3a4258629296ac27eec73699bfa40eb3
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 15:03:26 +0300

    Create pet entity and define its properties

---
+++ ServerApp/WebApi/Entities/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Pet
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
