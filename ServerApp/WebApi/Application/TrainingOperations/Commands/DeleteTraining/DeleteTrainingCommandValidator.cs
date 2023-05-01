2e6c5c87107c9eb06f764b61246d88215a8a7ddf
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 14:02:22 +0300

    Create validator for delete training command

---
+++ ServerApp/WebApi/Application/TrainingOperations/Commands/DeleteTraining/DeleteTrainingCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.TrainingOperations.Commands.DeleteTraining
+{
+    public class DeleteTrainingCommandValidator : AbstractValidator<DeleteTrainingCommand>
+    {
+        public DeleteTrainingCommandValidator()
+        {
+            RuleFor(command => command.TrainingId).GreaterThan(0);
+        }
+    }
+}
