f6431e1da741e66c348d4eaa89253b904abb3913
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 15:08:11 +0300

    Add genre property

--- ServerApp/WebApi/Entities/Pet.cs
+++ ServerApp/WebApi/Entities/Pet.cs
@@ -12,6 +12,8 @@ namespace WebApi.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
