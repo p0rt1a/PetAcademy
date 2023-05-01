90bf6b864947f09d635f5f96664cf491792a9e12
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 13:36:46 +0300

    Create update training command

---
+++ ServerApp/WebApi/Application/TrainingOperations/Commands/UpdateTraining/UpdateTrainingCommand.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.TrainingOperations.Commands.UpdateTraining
+{
+    public class UpdateTrainingCommand
+    {
+        private readonly IAcademyDbContext _dbContext;
+        public int TrainingId { get; set; }
+        public UpdateTrainingModel Model { get; set; }
+
+        public UpdateTrainingCommand(IAcademyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Handle()
+        {
+            var training = _dbContext.Trainings.SingleOrDefault(x => x.Id == TrainingId);
+
+            if (training is null)
+                throw new InvalidOperationException("Güncellenecek eğitim bulunamadı");
+
+            training.Description = string.IsNullOrEmpty(Model.Description.Trim()) ? training.Description : Model.Description;
+            training.Price = Model.Price > 0 ? Model.Price : training.Price;
+
+            _dbContext.SaveChanges();
+        }
+    }
+
+    public class UpdateTrainingModel
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
