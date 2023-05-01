277e01c7dc72e7fb0a0a00f7334f98a198d72d6c
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-01 21:07:34 +0300

    Create logger services

---
+++ ServerApp/WebApi/Services/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Services
+{
+    public class ConsoleLogger : ILoggerService
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
