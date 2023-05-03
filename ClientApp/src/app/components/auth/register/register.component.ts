c898af0ad52ca897be371aa50b6af98c01f1b2b0
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-03 20:54:41 +0300

    Create custom form css and use it in register section

---
+++ ClientApp/src/app/components/auth/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { RegisterModel } from 'src/app/models/RegisterModel';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  registerModel: RegisterModel = new RegisterModel('', '', '', '');
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
