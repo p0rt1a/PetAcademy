c70246745ffb9d416579052522bce39678d0e5a3
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-03 20:25:14 +0300

    Create routings for register component

--- ClientApp/src/routes.ts
+++ ClientApp/src/routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TrainingDetailComponent } from './app/components/trainings/training-detail/training-detail.component';
 import { TrainingsComponent } from './app/components/trainings/trainings.component';
 import { CreateEnrollmentComponent } from './app/components/enrollments/create-enrollment/create-enrollment.component';
+import { RegisterComponent } from './app/components/auth/register/register.component';
 
 export const appRoutes: Routes = [
   { path: '', component: TrainingsComponent },
   { path: 'training-detail', component: TrainingDetailComponent },
   { path: 'create-enrollment', component: CreateEnrollmentComponent },
+  { path: 'register', component: RegisterComponent },
 ];
