0f3629c0a466dfdc2275a68290d896c6a5d53dda
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-03 15:02:11 +0300

    Add create enrollment component routing

--- ClientApp/src/routes.ts
+++ ClientApp/src/routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TrainingDetailComponent } from './app/components/trainings/training-detail/training-detail.component';
 import { TrainingsComponent } from './app/components/trainings/trainings.component';
+import { CreateEnrollmentComponent } from './app/components/enrollments/create-enrollment/create-enrollment.component';
 
 export const appRoutes: Routes = [
   { path: '', component: TrainingsComponent },
   { path: 'training-detail', component: TrainingDetailComponent },
+  { path: 'create-enrollment', component: CreateEnrollmentComponent },
 ];
