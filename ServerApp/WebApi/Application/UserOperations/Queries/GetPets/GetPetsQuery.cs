6cd1d19a18d9f043e147411a14a681843bf92cbf
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-04 21:08:31 +0300

    Create get user pets operation and its methods

---
+++ ServerApp/WebApi/Application/UserOperations/Queries/GetPets/GetPetsQuery.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.UserOperations.Queries.GetPets
+{
+    public class GetPetsQuery
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public int UserId { get; set; }
+
+        public GetPetsQuery(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public List<PetViewModel> Handle()
+        {
+            var pets = _dbContext.Pets
+                .Include(x => x.Genre)
+                .Where(x => x.UserId == UserId)
+                .ToList<Pet>();
+
+            var vm = _mapper.Map<List<PetViewModel>>(pets);
+
+            return vm;
+        }
+    }
+
+    public class PetViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Genre { get; set; }
+    }
+}
