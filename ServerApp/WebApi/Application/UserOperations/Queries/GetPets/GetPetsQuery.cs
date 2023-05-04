a4855514be95b91be3e971cd2b398c6b70a8d6a2
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-04 22:09:04 +0300

    Change petViewModel to userPetViewModel and its properties

--- ServerApp/WebApi/Application/UserOperations/Queries/GetPets/GetPetsQuery.cs
+++ ServerApp/WebApi/Application/UserOperations/Queries/GetPets/GetPetsQuery.cs
@@ -21,24 +21,22 @@ namespace WebApi.Application.UserOperations.Queries.GetPets
             _mapper = mapper;
         }
 
-        public List<PetViewModel> Handle()
+        public List<UserPetViewModel> Handle()
         {
             var pets = _dbContext.Pets
                 .Include(x => x.Genre)
                 .Where(x => x.UserId == UserId)
                 .ToList<Pet>();
 
-            var vm = _mapper.Map<List<PetViewModel>>(pets);
+            var vm = _mapper.Map<List<UserPetViewModel>>(pets);
 
             return vm;
         }
     }
 
-    public class PetViewModel
+    public class UserPetViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
-        public string Genre { get; set; }
     }
 }
