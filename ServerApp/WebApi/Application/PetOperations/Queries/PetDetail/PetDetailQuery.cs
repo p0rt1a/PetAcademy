a9729870d8cb6359f9b7ab048b9d703a8dbe4ff3
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-04 22:10:15 +0300

    Create get pet detail query operation

---
+++ ServerApp/WebApi/Application/PetOperations/Queries/PetDetail/PetDetailQuery.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.PetOperations.Queries.PetDetail
+{
+    public class PetDetailQuery
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public int PetId { get; set; }
+
+        public PetDetailQuery(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public PetViewModel Handle()
+        {
+            var pet = _dbContext.Pets
+                .Include(x => x.Genre)
+                .SingleOrDefault(x => x.Id == PetId);
+
+            if (pet is null)
+                throw new InvalidOperationException("Evcil hayvan bulunamadı!");
+
+            var vm = _mapper.Map<PetViewModel>(pet);
+
+            return vm;
+        }
+    }
+
+    public class PetViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Genre { get; set; }
+    }
+}
