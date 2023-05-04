248c0921586073a115e387cce5ade2b8c37b8b28
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-04 19:15:31 +0300

    Create user controller

---
+++ ServerApp/WebApi/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]s")]
+    public class UserController : ControllerBase
+    {
+        private readonly IAcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserController(IAcademyDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserDetail(int id)
+        {
+            return Ok();
+        }
+    }
+}
