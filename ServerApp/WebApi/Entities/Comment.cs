dc3104adcf29f42ae1938b49160b959a17112808
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-07 15:21:35 +0300

    Create comment entity and set its properties

---
+++ ServerApp/WebApi/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Comment
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int TrainingId { get; set; }
+        public Training Training { get; set; }
+    }
+}
