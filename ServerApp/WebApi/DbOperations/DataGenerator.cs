dfb27f9f3253b0c861ef6176e2184881436fc937
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-07 15:20:45 +0300

    Create comment datas

--- ServerApp/WebApi/DbOperations/DataGenerator.cs
+++ ServerApp/WebApi/DbOperations/DataGenerator.cs
@@ -154,6 +154,41 @@ namespace WebApi.DbOperations
                         }
                     );
 
+                context.Comments.AddRange(
+                        new Comment()
+                        {
+                            Body = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia laudantium eius dignissimos laborum mollitia " +
+                            "iusto animi laboriosam exercitationem inventore.",
+                            Date = DateTime.Now.Date,
+                            UserId = 2,
+                            TrainingId = 1
+                        },
+                        new Comment()
+                        {
+                            Body = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia laudantium eius dignissimos laborum mollitia " +
+                            "iusto animi laboriosam exercitationem inventore.",
+                            Date = DateTime.Now.Date,
+                            UserId = 2,
+                            TrainingId = 2
+                        },
+                        new Comment()
+                        {
+                            Body = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia laudantium eius dignissimos laborum mollitia " +
+                            "iusto animi laboriosam exercitationem inventore.",
+                            Date = DateTime.Now.Date,
+                            UserId = 1,
+                            TrainingId = 2
+                        },
+                        new Comment()
+                        {
+                            Body = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officia laudantium eius dignissimos laborum mollitia " +
+                            "iusto animi laboriosam exercitationem inventore.",
+                            Date = DateTime.Now.Date,
+                            UserId = 1,
+                            TrainingId = 1
+                        }
+                    );
+
                 context.SaveChanges();
             }
         }
