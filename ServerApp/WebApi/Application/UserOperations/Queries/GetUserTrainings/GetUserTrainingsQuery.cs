1a7a9db7617ca009d2454199c549bb10372a3779
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-07 20:35:27 +0300

    Add max pet count property to view model

---
+++ ServerApp/WebApi/Application/UserOperations/Queries/GetUserTrainings/GetUserTrainingsQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Application.TrainingOperations.Queries.GetTrainings;
+using WebApi.DbOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.UserOperations.Queries.GetUserTrainings
+{
+    public class GetUserTrainingsQuery
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public int UserId { get; set; }
+
+        public GetUserTrainingsQuery(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public List<TrainingViewModel> Handle()
+        {
+            var trainings = _dbContext.Trainings
+                .Include(x => x.Genre)
+                .Where(x => x.UserId == UserId)
+                .ToList<Training>();
+
+            var vm = _mapper.Map<List<TrainingViewModel>>(trainings);
+
+            return vm;
+        }
+    }
+}
