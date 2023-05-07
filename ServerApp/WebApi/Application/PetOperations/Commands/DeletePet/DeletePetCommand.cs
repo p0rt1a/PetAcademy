2e1506b03322e112ff57c3d2ad65957922769740
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-07 15:22:21 +0300

    Create delete pet operations

---
+++ ServerApp/WebApi/Application/PetOperations/Commands/DeletePet/DeletePetCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.PetOperations.Commands.DeletePet
+{
+    public class DeletePetCommand
+    {
+        private readonly IAcademyDbContext _dbContext;
+        public int PetId { get; set; }
+
+        public DeletePetCommand(IAcademyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Handle()
+        {
+            var pet = _dbContext.Pets.SingleOrDefault(x => x.Id == PetId);
+
+            if (pet is null)
+                throw new InvalidOperationException("Evcil hayvan bulunamadı!");
+
+            _dbContext.Pets.Remove(pet);
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ ServerApp/WebApi/Application/PetOperations/Commands/DeletePet/DeletePetCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.PetOperations.Commands.DeletePet
+{
+    public class DeletePetCommandValidator  :AbstractValidator<DeletePetCommand>
+    {
+        public DeletePetCommandValidator()
+        {
+            RuleFor(command => command.PetId).GreaterThan(0);
+        }
+    }
+}
