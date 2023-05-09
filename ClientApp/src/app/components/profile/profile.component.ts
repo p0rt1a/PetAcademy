7e8bc871aabf2734e98ec5c78fb75e1cd9c9d039
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-09 12:49:12 +0300

    Create profile component and its methods

--- ClientApp/src/app/components/profile/profile.component.ts
+++ ClientApp/src/app/components/profile/profile.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { CreatePetModel } from 'src/app/models/CreatePetModel';
 import { GenreModel } from 'src/app/models/GenreModel';
 import { PetViewModel } from 'src/app/models/PetViewModel';
 import { UpdatePetModel } from 'src/app/models/UpdatePetModel';
+import { UpdateUserModel } from 'src/app/models/UpdateUserModel';
 import { UserDetailViewModel } from 'src/app/models/UserDetailViewModel';
 import { UserPetViewModel } from 'src/app/models/UserPetViewModel';
 import { AuthService } from 'src/app/services/auth.service';
@@ -17,44 +23,67 @@ import { UsersService } from 'src/app/services/users.service';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  user: UserDetailViewModel = new UserDetailViewModel('', '', '');
-  pets: UserPetViewModel[] = [];
-  pet: PetViewModel = new PetViewModel(0, '', 0, '');
-  updatePetModel: UpdatePetModel = new UpdatePetModel(0, '', 0);
-  isPetIdSelected: boolean = false;
-  genres: GenreModel[] = [];
+  createPetForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    genre: new FormControl(),
+    age: new FormControl(),
+  });
   createPetModel: CreatePetModel = new CreatePetModel('', 0, 0, 0);
+  updatePetModel: UpdatePetModel = new UpdatePetModel(0, '', 0);
+  isUpdatingPet: boolean = false;
+  pets: UserPetViewModel[] = [];
+  genres: GenreModel[] = [];
+  pet: PetViewModel = new PetViewModel(0, '', 0, '');
+  petUpdateModel: UpdatePetModel = new UpdatePetModel(0, '', 0);
+  updatePetForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    age: new FormControl(),
+  });
+  user: UserDetailViewModel = new UserDetailViewModel('', '', '');
+  updateUserModel: UpdateUserModel = new UpdateUserModel('');
 
-  selectedControl: FormControl = new FormControl('value', Validators.min(0));
+  updateUserForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+  });
 
   constructor(
-    private authService: AuthService,
+    private formBuilder: FormBuilder,
     private usersService: UsersService,
-    private petsService: PetsService,
-    private genresService: GenresService
+    private authService: AuthService,
+    private genresService: GenresService,
+    private petsService: PetsService
   ) {}
 
   ngOnInit(): void {
+    this.createPetForm = this.formBuilder.group({
+      name: [, [Validators.required, Validators.minLength(2)]],
+      genre: [, Validators.required],
+      age: [, [Validators.required, Validators.min(0)]],
+    });
+
+    this.updateUserForm = this.formBuilder.group({
+      email: [, Validators.email],
+    });
+
     this.usersService
       .getUserDetail(this.authService.getUserId())
       .subscribe((response) => {
         this.user = response;
       });
 
+    this.genresService.getGenres().subscribe((response) => {
+      this.genres = response;
+    });
+
     this.usersService
       .getUserPets(this.authService.getUserId())
       .subscribe((response) => {
         this.pets = response;
       });
-
-    this.genresService.getGenres().subscribe((response) => {
-      this.genres = response;
-    });
   }
 
   getPetDetail(id: any) {
     this.petsService.getPetDetail(id).subscribe((response) => {
-      console.log(response);
       this.pet = response;
     });
   }
@@ -62,23 +91,58 @@ export class ProfileComponent implements OnInit {
   updatePet(id: any) {
     this.updatePetModel.userId = this.authService.getUserId();
 
-    this.petsService
-      .updatePet(id, this.updatePetModel)
-      .subscribe((response) => {
-        if (response.status == 200) {
-          //TODO: Create your OK logic
-        }
-      });
+    this.petsService.updatePet(id, this.updatePetModel).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  updateUser(form: FormGroup) {
+    this.updateUserModel.email = form.value.email;
+
+    let userId: number = this.authService.getUserId();
+    this.usersService.updateUser(userId, this.updateUserModel).subscribe(
+      (response) => {
+        //TODO: Create success method here:
+        console.log('Güncelleme başarılı');
+      },
+      (error) => {
+        //TODO: Create fail method here:
+        console.log(error.error.error);
+      }
+    );
+  }
+
+  createPet(form: FormGroup) {
+    this.createPetModel.name = form.value.name;
+    this.createPetModel.age = form.value.age;
+    this.createPetModel.genreId = parseInt(form.value.genre);
+    this.createPetModel.userId = this.authService.getUserId();
+
+    this.petsService.createPet(this.createPetModel).subscribe(
+      (response) => {
+        //TODO: Create successfull method here:
+      },
+      (error) => {
+        //TODO: Create failed method here:
+      }
+    );
   }
 
   deletePet(id: any) {
-    this.petsService.deletePet(id).subscribe((response) => {
-      console.log(response);
-    });
-  }
-
-  createPet() {
-    this.createPetModel.userId = this.authService.getUserId();
-    console.log(this.createPetModel);
+    this.petsService.deletePet(id).subscribe(
+      (response) => {
+        if (response.status == 200) {
+          console.log('Silme işlemi başarılı');
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
