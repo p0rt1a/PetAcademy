c7e2e06e4115ecaf93ae1d2059db41749c6903e0
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 16:34:35 +0300

    Create validator for get training pets query

---
+++ ServerApp/WebApi/Application/TrainingOperations/Queries/GetTrainingPets/GetTrainingPetsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.TrainingOperations.Queries.GetTrainingPets
+{
+    public class GetTrainingPetsQueryValidator : AbstractValidator<GetTrainingPetsQuery>
+    {
+        public GetTrainingPetsQueryValidator()
+        {
+            RuleFor(query => query.TrainingId).GreaterThan(0);
+        }
+    }
+}
