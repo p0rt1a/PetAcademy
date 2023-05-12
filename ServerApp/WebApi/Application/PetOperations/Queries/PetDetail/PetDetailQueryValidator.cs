a3526a996548e1451b37a2b15669f570f98306d6
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 19:43:09 +0300

    Create validator for pet detail query

---
+++ ServerApp/WebApi/Application/PetOperations/Queries/PetDetail/PetDetailQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.PetOperations.Queries.PetDetail
+{
+    public class PetDetailQueryValidator : AbstractValidator<PetDetailQuery>
+    {
+        public PetDetailQueryValidator()
+        {
+            RuleFor(query => query.PetId).GreaterThan(0); 
+        }
+    }
+}
