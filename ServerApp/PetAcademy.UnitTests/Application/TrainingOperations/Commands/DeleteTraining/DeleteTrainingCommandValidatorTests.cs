982d3aaf700c4328538bcbc53842c8a6a05fa039
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 19:34:23 +0300

    Create update training command tests

---
+++ ServerApp/PetAcademy.UnitTests/Application/TrainingOperations/Commands/DeleteTraining/DeleteTrainingCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.TrainingOperations.Commands.DeleteTraining;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.TrainingOperations.Commands.DeleteTraining
+{
+    public class DeleteTrainingCommandValidatorTests
+    {
+        [Fact]
+        public void WhenInvalidTrainingIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            DeleteTrainingCommand command = new(null);
+            command.TrainingId = 0;
+
+            DeleteTrainingCommandValidator validator = new();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenValidInputsAreGiven_Validator_ShouldNotBeReturnError()
+        {
+            DeleteTrainingCommand command = new(null);
+            command.TrainingId = 1;
+
+            DeleteTrainingCommandValidator validator = new();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
---
+++ ServerApp/PetAcademy.UnitTests/Application/TrainingOperations/Commands/UpdateTraining/UpdateTrainingCommandTests.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using PetAcademy.UnitTests.TestSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.TrainingOperations.Commands.UpdateTraining;
+using WebApi.DbOperations;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.TrainingOperations.Commands.UpdateTraining
+{
+    public class UpdateTrainingCommandTests  :IClassFixture<CommonTestFixture>
+    {
+        private readonly AcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UpdateTrainingCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenDoesNotExistTrainingIdIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            UpdateTrainingCommand command = new(_context);
+            command.TrainingId = 0;
+
+            FluentActions.Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Güncellenecek eğitim bulunamadı");
+        }
+
+        [Fact]
+        public void WhenAlreadyExistTrainingIdIsGiven_Training_ShouldBeUpdated()
+        {
+            UpdateTrainingCommand command = new(_context);
+            command.TrainingId = 1;
+
+            command.Model = new UpdateTrainingModel()
+            {
+                Description = "WhenAlreadyExistTrainingIdIsGiven_Training_ShouldBeUpdated",
+                Price = 200,
+                MaxPetCount = 1
+            };
+
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            var training = _context.Trainings.SingleOrDefault(x => x.Id == command.TrainingId);
+            training.Description.Should().Be("WhenAlreadyExistTrainingIdIsGiven_Training_ShouldBeUpdated");
+            training.Price.Should().Be(200);
+            training.MaxPetCount.Should().Be(1);
+        }
+    }
+}
