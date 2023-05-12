1550db4a345d70371e29e8a39e28fc162517237f
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 19:53:59 +0300

    Creae pet detail query validator tests

---
+++ ServerApp/PetAcademy.UnitTests/Application/PetOperations/Queries/PetDetail/PetDetailQueryValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.PetOperations.Queries.PetDetail;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.PetOperations.Queries.PetDetail
+{
+    public class PetDetailQueryValidatorTests
+    {
+        [Fact]
+        public void WhenInvalidPetIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            PetDetailQuery query = new(null, null);
+            query.PetId = 0;
+
+            PetDetailQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenValidInputsAreGiven_Validator_ShouldNotBeReturnError()
+        {
+            PetDetailQuery query = new(null, null);
+            query.PetId = 1;
+
+            PetDetailQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().Equals(0);
+        }
+    }
+}
