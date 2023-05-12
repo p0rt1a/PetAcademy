34436df30e06f6a5501d0ae54b225e44cc5645c7
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-13 00:29:42 +0300

    Create validator for create comment command

---
+++ ServerApp/WebApi/Application/CommentOperations/Commands/CreateComment/CreateCommentCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.CommentOperations.Commands.CreateComment
+{
+    public class CreateCommentCommandValidator: AbstractValidator<CreateCommentCommand>
+    {
+        public CreateCommentCommandValidator()
+        {
+            RuleFor(command => command.Model.UserId).GreaterThan(0);
+            RuleFor(command => command.Model.TrainingId).GreaterThan(0);
+        }
+    }
+}
