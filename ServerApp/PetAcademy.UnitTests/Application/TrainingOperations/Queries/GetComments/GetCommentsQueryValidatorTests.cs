aeff0e350f15bcbba8464b8a8b73c0069c0310ca
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 17:50:33 +0300

    Create get comments query validator tests

---
+++ ServerApp/PetAcademy.UnitTests/Application/TrainingOperations/Queries/GetComments/GetCommentsQueryValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using PetAcademy.UnitTests.TestSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.TrainingOperations.Queries.GetComments;
+using WebApi.DbOperations;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.TrainingOperations.Queries.GetComments
+{
+    public class GetCommentsQueryValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        [Fact]
+        public void WhenınvalidTrainingIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            GetCommentsQuery query = new(null, null);
+            query.TrainingId = 0;
+
+            GetCommentsQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
