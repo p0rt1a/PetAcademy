6149b21d6a19e2b6a4acf2c0335fe4279f963ec4
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 23:08:34 +0300

    Create delete pet command tests

---
+++ ServerApp/PetAcademy.UnitTests/Application/PetOperations/Command/DeletePet/DeletePetCommandTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using FluentValidation;
+using PetAcademy.UnitTests.TestSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.PetOperations.Commands.DeletePet;
+using WebApi.DbOperations;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.PetOperations.Command.DeletePet
+{
+    public class DeletePetCommandTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly AcademyDbContext _context;
+
+        public DeletePetCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+        }
+
+        [Fact]
+        public void WhenDoesNotExistPetIdIsGiven_InvalidOperationException_ShouldBeReturnError()
+        {
+            DeletePetCommand command = new(_context);
+            command.PetId = 0;
+
+            FluentActions.Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Evcil hayvan bulunamadı!");
+        }
+
+        [Fact]
+        public void WhenAlreadyExistPetIdIsGiven_Pet_ShouldBeDeleted()
+        {
+            DeletePetCommand command = new(_context);
+            command.PetId = _context.Pets.Count();
+
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            var pet = _context.Pets.SingleOrDefault(x => x.Id == command.PetId);
+            pet.Should().BeNull();
+        }
+    }
+}
