bd181882fc462e3b9c019df49f62e65becfe4159
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 17:58:19 +0300

    Create get training pets query validator tests

--- ServerApp/PetAcademy.UnitTests/Application/TrainingOperations/Queries/GetTrainingPets/GetTrainingPetsQueryValidatorTests.cs
+++ ServerApp/PetAcademy.UnitTests/Application/TrainingOperations/Queries/GetTrainingPets/GetTrainingPetsQueryValidatorTests.cs
@@ -6,21 +6,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebApi.Application.TrainingOperations.Queries.GetComments;
+using WebApi.Application.TrainingOperations.Queries.GetTrainingPets;
 using WebApi.DbOperations;
 using Xunit;
 
-namespace PetAcademy.UnitTests.Application.TrainingOperations.Queries.GetComments
+namespace PetAcademy.UnitTests.Application.TrainingOperations.Queries.GetTrainingPets
 {
-    public class GetCommentsQueryValidatorTests
+    public class GetTrainingPetsQueryValidatorTests
     {
         [Fact]
-        public void WhenınvalidTrainingIdIsGiven_Validator_ShouldBeReturnError()
+        public void WhenInvalidTrainingIdIsGiven_Validator_ShouldBeReturnError()
         {
-            GetCommentsQuery query = new(null, null);
+            GetTrainingPetsQuery query = new(null, null);
             query.TrainingId = 0;
 
-            GetCommentsQueryValidator validator = new();
+            GetTrainingPetsQueryValidator validator = new();
             var result = validator.Validate(query);
 
             result.Errors.Count.Should().BeGreaterThan(0);
