cf5aadc6227f7e1e14fbbefd4434ed8e85383a1a
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 23:11:32 +0300

    Create delete pet command validator tests clear

---
+++ ServerApp/PetAcademy.UnitTests/Application/PetOperations/Command/DeletePet/DeletePetCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.PetOperations.Commands.DeletePet;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.PetOperations.Command.DeletePet
+{
+    public class DeletePetCommandValidatorTests
+    {
+        [Fact]
+        public void WhenInvalidPetIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            DeletePetCommand command = new(null);
+            command.PetId = 0;
+
+            DeletePetCommandValidator validator = new();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenValidPetIdIsGiven_Validator_ShouldNotBeReturnError()
+        {
+            DeletePetCommand command = new(null);
+            command.PetId = 1;
+
+            DeletePetCommandValidator validator = new();
+            var result = validator.Validate(command);
+
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
