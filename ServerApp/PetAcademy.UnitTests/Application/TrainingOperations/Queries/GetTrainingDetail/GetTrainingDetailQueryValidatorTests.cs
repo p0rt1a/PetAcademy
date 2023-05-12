bfe9e5a1a0e432adc142835664b423decd5b3227
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-12 16:33:12 +0300

    Create get training detail query validator tests

---
+++ ServerApp/PetAcademy.UnitTests/Application/TrainingOperations/Queries/GetTrainingDetail/GetTrainingDetailQueryValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.TrainingOperations.Queries.GetTrainingDetail;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.TrainingOperations.Queries.GetTrainingDetail
+{
+    public class GetTrainingDetailQueryValidatorTests
+    {
+        [Fact]
+        public void WhenInvalidTrainingIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            GetTrainingDetailQuery query = new(null, null);
+            query.TrainingId = 0;
+
+            GetTrainingDetailQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+    }
+}
