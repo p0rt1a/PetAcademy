9c1c5ed8e7a9405b9316705b97a5e903ef4caf42
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-14 23:56:26 +0300

    Create validator for update user command

---
+++ ServerApp/WebApi/Application/UserOperations/Commands/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.UserOperations.Commands.UpdateUser
+{
+    public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserCommandValidator()
+        {
+            RuleFor(command => command.UserId).GreaterThan(0);
+            RuleFor(command => command.Model.Email.Trim()).MinimumLength(5);
+        }
+    }
+}
