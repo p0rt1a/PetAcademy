6b8c582b44b7fd5384390fade62ac0fdc73ed552
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-15 13:18:05 +0300

    Create certificate entity

---
+++ ServerApp/WebApi/Entities/Certificate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Certificate
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int TrainingId { get; set; }
+        public Training Training { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
