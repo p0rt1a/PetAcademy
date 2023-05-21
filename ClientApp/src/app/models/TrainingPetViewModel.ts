2fc7bdfede814a48d1e0e8005d87dc96aaef8647
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-22 00:46:26 +0300

    Create create certificate model

--- ClientApp/src/app/models/TrainingPetViewModel.ts
+++ ClientApp/src/app/models/TrainingPetViewModel.ts
@@ -1,10 +1,18 @@
 export class TrainingPetViewModel {
+  petId: number;
   name: string;
   age: number;
   genre: string;
   userEmail: string;
 
-  constructor(name: string, age: number, genre: string, userEmail: string) {
+  constructor(
+    petId: number,
+    name: string,
+    age: number,
+    genre: string,
+    userEmail: string
+  ) {
+    this.petId = petId;
     this.name = name;
     this.age = age;
     this.genre = genre;
