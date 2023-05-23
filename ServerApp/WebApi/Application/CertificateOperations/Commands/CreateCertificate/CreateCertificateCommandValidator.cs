255487d41c73afffddbb338fc64fb3b9ae25c8bb
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 16:12:27 +0300

    Create validator for create certificate command

---
+++ ServerApp/WebApi/Application/CertificateOperations/Commands/CreateCertificate/CreateCertificateCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.CertificateOperations.Commands.CreateCertificate
+{
+    public class CreateCertificateCommandValidator : AbstractValidator<CreateCertificateCommand>
+    {
+        public CreateCertificateCommandValidator()
+        {
+            RuleFor(command => command.Model.PetId).GreaterThan(0);
+            RuleFor(command => command.Model.TrainingId).GreaterThan(0);
+        }
+    }
+}
