b9cdbfcdde55d595fa5fad7dc17936dfb8699218
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 16:10:04 +0300

    Create validator for pet certificates query

---
+++ ServerApp/WebApi/Application/PetOperations/Queries/PetCertificates/PetCertificatesQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.PetOperations.Queries.PetCertificates
+{
+    public class PetCertificatesQueryValidator : AbstractValidator<PetCertificatesQuery>
+    {
+        public PetCertificatesQueryValidator()
+        {
+            RuleFor(query => query.PetId).GreaterThan(0);
+        }
+    }
+}
