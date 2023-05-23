e655ab9c47a017c9c6fd5c23380772c34f83cdfd
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 16:38:26 +0300

    Create tests for get pets query validator

---
+++ ServerApp/PetAcademy.UnitTests/Application/UserOperations/Commands/GetPets/GetPetsQueryValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.UserOperations.Queries.GetPets;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.UserOperations.Commands.GetPets
+{
+    public class GetPetsQueryValidatorTests
+    {
+        [Fact]
+        public void WhenInvalidUserIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            GetPetsQuery query = new(null, null);
+            query.UserId = 0;
+
+            GetPetsQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenInvalidUserIdIsGiven_Validator_ShouldNotBeReturnError()
+        {
+            GetPetsQuery query = new(null, null);
+            query.UserId = 1;
+
+            GetPetsQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().Equals(0);
+        }
+    }
+}
