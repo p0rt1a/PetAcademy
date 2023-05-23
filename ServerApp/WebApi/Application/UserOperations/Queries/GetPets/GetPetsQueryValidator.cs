9f3227dd3f8429abae86de9772abad97b7181009
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 16:06:44 +0300

    Create validator for get pets query

---
+++ ServerApp/WebApi/Application/UserOperations/Queries/GetPets/GetPetsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.UserOperations.Queries.GetPets
+{
+    public class GetPetsQueryValidator : AbstractValidator<GetPetsQuery>
+    {
+        public GetPetsQueryValidator()
+        {
+            RuleFor(query => query.UserId).GreaterThan(0);
+        }
+    }
+}
