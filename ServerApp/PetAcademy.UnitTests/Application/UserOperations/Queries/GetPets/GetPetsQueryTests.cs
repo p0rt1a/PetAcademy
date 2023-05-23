8bc4b8dad76fbe2583bf473eabf47c4eab13108a
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 17:29:17 +0300

    Create get pets query validator tests

---
+++ ServerApp/PetAcademy.UnitTests/Application/UserOperations/Queries/GetPets/GetPetsQueryTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using PetAcademy.UnitTests.TestSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.UserOperations.Queries.GetPets;
+using WebApi.DbOperations;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.UserOperations.Queries.GetPets
+{
+    public class GetPetsQueryTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly AcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPetsQueryTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenDoesntExistUserIdIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            GetPetsQuery query = new(_context, _mapper);
+            query.UserId = 0;
+
+            FluentActions.Invoking(() => query.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kullanıcı mevcut değil!");
+        }
+
+        [Fact]
+        public void WhenAlreadyExistUserIdIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            GetPetsQuery query = new(_context, _mapper);
+            query.UserId = 1;
+
+            var result = FluentActions.Invoking(() => query.Handle()).Invoke();
+
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ ServerApp/PetAcademy.UnitTests/Application/UserOperations/Queries/GetPets/GetPetsQueryValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.UserOperations.Queries.GetPets;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.UserOperations.Queries.GetPets
+{
+    public class GetPetsQueryValidatorTests
+    {
+        [Fact]
+        public void WhenInvalidUserIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            GetPetsQuery query = new(null, null);
+            query.UserId = 0;
+
+            GetPetsQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenValidUserIdIsGiven_Validator_ShouldBeReturnError()
+        {
+            GetPetsQuery query = new(null, null);
+            query.UserId = 1;
+
+            GetPetsQueryValidator validator = new();
+            var result = validator.Validate(query);
+
+            result.Errors.Count.Should().Equals(0);
+        }
+    }
+}
