d2c31163f96f0f1671736f72a75c84a786a91007
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 17:58:21 +0300

    Create delete enrollment operation

---
+++ ServerApp/WebApi/Application/EnrollmentOperations/Commands/DeleteEnrollment/DeleteEnrollmentCommand.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DbOperations;
+
+namespace WebApi.Application.EnrollmentOperations.Commands.DeleteEnrollment
+{
+    public class DeleteEnrollmentCommand
+    {
+        private readonly IAcademyDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public int TrainingId { get; set; }
+        public int PetId { get; set; }
+
+        public DeleteEnrollmentCommand(IAcademyDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var enrollment = _dbContext.Enrollments
+                .FirstOrDefault(x => x.PetId == PetId && x.TrainingId == TrainingId);
+
+            if (enrollment is null)
+                throw new InvalidOperationException("Evcil hayvan bu eğitime üye değil!");
+
+            _dbContext.Enrollments.Remove(enrollment);
+            _dbContext.SaveChanges();
+        }
+    }
+}
