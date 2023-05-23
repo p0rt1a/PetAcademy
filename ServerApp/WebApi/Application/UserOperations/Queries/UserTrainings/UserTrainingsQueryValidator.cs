6d365cf6219a03b12f30d97165de45aff1504560
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 16:08:09 +0300

    Create validator for user trainings query

---
+++ ServerApp/WebApi/Application/UserOperations/Queries/UserTrainings/UserTrainingsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.UserOperations.Queries.UserTrainings
+{
+    public class UserTrainingsQueryValidator : AbstractValidator<UserTrainingsQuery>
+    {
+        public UserTrainingsQueryValidator()
+        {
+            RuleFor(query => query.UserId).GreaterThan(0);
+        }
+    }
+}
