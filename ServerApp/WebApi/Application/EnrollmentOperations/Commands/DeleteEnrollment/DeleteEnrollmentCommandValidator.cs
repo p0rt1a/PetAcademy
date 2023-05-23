84ad6009ecd6f10c6c6130075220ea50b437239b
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 18:00:58 +0300

    Create validator for delete enrollment command

---
+++ ServerApp/WebApi/Application/EnrollmentOperations/Commands/DeleteEnrollment/DeleteEnrollmentCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Application.EnrollmentOperations.Commands.DeleteEnrollment
+{
+    public class DeleteEnrollmentCommandValidator : AbstractValidator<DeleteEnrollmentCommand>
+    {
+        public DeleteEnrollmentCommandValidator()
+        {
+            RuleFor(command => command.Model.PetId).GreaterThan(0);
+            RuleFor(command => command.Model.TrainingId).GreaterThan(0);
+        }
+    }
+}
