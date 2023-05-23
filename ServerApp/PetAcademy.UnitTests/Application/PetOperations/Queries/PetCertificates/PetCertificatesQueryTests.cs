bc3a5d5e921a42d3a41488a82e88fd5dd18c88cc
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-05-23 16:51:30 +0300

    Create tests for pet certificates query

---
+++ ServerApp/PetAcademy.UnitTests/Application/PetOperations/Queries/PetCertificates/PetCertificatesQueryTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using PetAcademy.UnitTests.TestSetup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Application.PetOperations.Queries.PetCertificates;
+using WebApi.DbOperations;
+using Xunit;
+
+namespace PetAcademy.UnitTests.Application.PetOperations.Queries.PetCertificates
+{
+    public class PetCertificatesQueryTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly AcademyDbContext _context;
+        private readonly IMapper _mapper;
+
+        public PetCertificatesQueryTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.Context;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenDoesntExistPetIdIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            PetCertificatesQuery query = new(_context, _mapper);
+            query.PetId = 0;
+
+            FluentActions.Invoking(() => query.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Evcil hayvan bulunamadı!");
+        }
+
+        [Fact]
+        public void WhenAlreadyExistPetIdIsGiven_PetCertificates_ShouldBeReturn()
+        {
+            PetCertificatesQuery query = new(_context, _mapper);
+            query.PetId = 1;
+
+            var result = FluentActions.Invoking(() => query.Handle()).Invoke();
+
+            result.Should().NotBeNull();
+        }
+    }
+}
